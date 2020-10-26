0c1e00d236cbc69372f861c253bc645096df900a
Rodrigo Bastos <ro.p.bastos@gmail.com>
2020-10-25 23:20:24 -0300

    ultimo commit antes de enviar

--- etapa_4/test_batch.zsh
+++ etapa_4/test_batch.zsh
@@ -1,9 +1,8 @@
 #!/bin/zsh
 
-for ((i = 0; i < 73; i++)); do 
+for ((i = 0; i < 76; i++)); do 
   echo  'test' $i
   head -1 tests/t$i
-  code=$(head -1 tests/t$i | grep -Eo '[0-9]{1,4}')
   ./etapa4 < tests/t$i >> /dev/null
   echo $?
   echo ; done
