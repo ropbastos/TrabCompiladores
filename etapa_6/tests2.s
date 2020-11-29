0a0aa50180efa9754bd241795b9d8760d1f1ad52
ropbastos <ro.p.bastos@gmail.com>
2020-11-29 13:40:35 -0300

    soh salvando como ta antes deu tentar substituir o metodo chamada de funcao do iloc pelo uso de call do asm x86 - o do iloc da muita dor de kbeca pq as instrucoes tem tamanho diferente o que faz com que calcular o endereco de retorno seja dificil demais

--- etapa_6/tests2.s
+++ etapa_6/tests2.s
@@ -2,47 +2,74 @@
   .type main, @function
 L1:
   movq %rsp, %rbp
-  subq $56, %rsp
-  movq -24(%rbp), %rcx
-  movq %rcx, -40(%rbp)
-  movq -32(%rbp), %rcx
-  movq %rcx, -48(%rbp)
+  subq $40, %rsp
+  movq -24(%rbp), %rbx
+  movq %rbx, -40(%rbp)
+  movq $0, %rbx
+  movq %rbx, -48(%rbp)
+  movq $1, %rbx
+  movq %rbx, -56(%rbp)
   movq -40(%rbp), %rcx
-  movq %rcx, -56(%rbp)
-  movq -40(%rbp), %rbx
-  movq -48(%rbp), %rcx
+  movq $0, %rdx
+  cmp %rcx, %rdx
+  jne L3
+  je L2
+L2:
+  movq -48(%rbp), %rsi
+  movq %rsi, %rdi
+  movq %rdi, -32(%rbp)
+  movq -0(%rbp), %rdi
+  movq -8(%rbp), %rbx
+  movq -16(%rbp), %rcx
+  movq %rbx, %rsp
+  movq %rcx, %rbp
+  jmp *%rdi
+L3:
+  movq $2, %rdx
+  movq %rdx, -72(%rbp)
+L11:
+  movq -72(%rbp), %rsi
+  movq -40(%rbp), %rdi
+  cmp %rsi, %rdi
+  jge L9
+  jnge L10
+L9:
+  movq -48(%rbp), %rdx
+  movq -56(%rbp), %rsi
+  addq %rdx, %rsi
+  movq %rsi, -64(%rbp)
+  movq -56(%rbp), %rdi
+  movq %rdi, -48(%rbp)
+  movq -64(%rbp), %rbx
+  movq %rbx, -56(%rbp)
+  movq -72(%rbp), %rbx
+  movq $1, %rcx
   addq %rbx, %rcx
+  movq %rcx, -72(%rbp)
+  jmp L11
+L10:
+  movq -56(%rbp), %rcx
   movq -56(%rbp), %rdx
-  addq %rcx, %rdx
-  movq %rdx, %rsi
-  movq %rsi, -32(%rbp)
-  movq -0(%rbp), %rsi
-  movq -8(%rbp), %rdi
-  movq -16(%rbp), %rbx
-  movq %rdi, %rsp
-  movq %rbx, %rbp
-  jmp *%rsi
+  movq %rdx, -32(%rbp)
+  movq -0(%rbp), %rdx
+  movq -8(%rbp), %rsi
+  movq -16(%rbp), %rdi
+  movq %rsi, %rsp
+  movq %rdi, %rbp
+  jmp *%rdx
 main:
   pushq %rbp
   movq %rsp, %rbp
-  subq $72, %rsp
-  movq $2, %rdx
-  movq %rdx, -48(%rbp)
-  movq $10, %rsi
-  movq %rsi, -56(%rbp)
-  movq $8, %rdi
-  movq %rdi, -64(%rbp)
+  subq $56, %rsp
+  movq $9, %rcx
+  movq %rcx, -48(%rbp)
   lea 0(%rip), %rdi
-  addq $50, %rdi
+  addq $32, %rdi
   movq %rdi, -0(%rsp)
   movq %rsp, -8(%rsp)
   movq %rbp, -16(%rsp)
-  movq -56(%rbp), %rbx
-  movq %rbx, -24(%rsp)
-  movq -64(%rbp), %rcx
-  movq %rcx, -32(%rsp)
   movq -48(%rbp), %rdx
-  movq %rdx, -40(%rsp)
+  movq %rdx, -24(%rsp)
   jmp L1
   movq -32(%rsp), %rsi
   movq %rsi, -40(%rbp)
